b737208ce94555d70820ef07ad133b1599b1a7e7
Tony Chabi <anthonymadeya@gmail.com>
2016-01-15 12:01:00 -0600

    first commit

---
+++ movieQuotesApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace movieQuotesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int random = new Random().Next(3);
+            var fileCollection = new string[3]
+            {
+                "http://www.wavsource.com/snds_2016-01-10_6357263721580594/tv/commercials/gray_poop.wav", "http://www.wavsource.com/snds_2016-01-10_6357263721580594/movies/misc/wayne_monkeys.wav", "http://www.wavsource.com/snds_2016-01-10_6357263721580594/sfx/boo.wav"
+            };
+            var Player = new SoundPlayer(fileCollection[random]);
+            Player.Play();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ movieQuotesApp/SoundpPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace movieQuotesApp
+{
+    internal class SoundpPlayer
+    {
+        public SoundpPlayer()
+        {
+        }
+
+        internal void Play(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
